7dbe7519156a1c2b092e897441552a017ad1bd97
YutongHan1123 <42613354+YutongHan1123@users.noreply.github.com>
2020-03-14 00:30:29 -0400

    Add files via upload

---
+++ js/language.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+$(function() {
+  $( "#language-toggle" ).click(function(){
+    $( ".text-on" ).switchClass( "text-on", "text-off", 10 );
+    $( ".text-off" ).switchClass( "text-off", "text-on", 10 );
+  });
+});
+});
